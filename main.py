92ee71e33d3ce3ad4346c4ab5bbbde0aa5dfa185
Jjponvv <limbogeom@gmail.com>
2025-09-27 21:47:09 +0300

    Added many functions, I plane transform to Runge-Kutta method for better accuracy and performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #Import libraries
 import pygame as pg
-import math
+import math, datetime
 
 #init pygame
 pg.init()
@@ -12,7 +12,12 @@ sc = pg.display.set_mode(RES)
 #set nesesery constants
 G = 0.000000000066743
 SCALE = 5e7
-TIMESTEP = 60*15
+TIMESTEP = 3600*24*30/60
+
+start_date = datetime.datetime(2025, 1, 1)
+current_time_seconds = 0  
+
+font = pg.font.SysFont("Arial", 24)
 
 camera_x, camera_y = 0, 0
 
@@ -26,9 +31,16 @@ def screen_to_world(sx, sy):
     y = (sy - HEIGHT/2) * SCALE + camera_y
     return x, y
 
+def is_mouse_over(obj, mouse_pos, scale):
+    mx, my = mouse_pos
+    x, y = world_to_screen(obj.x, obj.y)
+    r = max(1, int(obj.radius * (5e8/scale)))
+    distance = math.hypot(mx - x, my - y)
+    return distance <= r
+
 #main class
 class Object_:
-    def __init__(self, x, y, radius, color, mass):
+    def __init__(self, x, y, radius, color, mass, trail=True, name=""):
       #set variables
         self.x = x
         self.y = y
@@ -39,17 +51,26 @@ class Object_:
         self.vy = 0
         self.fx = 0
         self.fy = 0
+        self.name = name
+        self.trail = trail
+        self.positions = []
 
-    def draw(self, sc):
-      #Drawing object
-        pg.draw.circle(sc, self.color, (int(self.x/SCALE+WIDTH/2), int(self.y/SCALE+HEIGHT/2)), self.radius)
+    def draw(self, sc, scale):
+        #Drawing object
+        x, y = world_to_screen(self.x, self.y)
+
+        if self.trail and len(self.positions) > 1:
+            points = [world_to_screen(px, py) for px, py in self.positions]
+            pg.draw.lines(sc, (255 - self.color[0], 255 - self.color[1], 255 - self.color[2]), False, points, 2)
+
+        pg.draw.circle(sc, self.color, (x, y), max(1, self.radius * (5e8 / scale)))
 
     #get attraction force
     def attraction(self, other):
         dx = other.x - self.x #delta x
         dy = other.y - self.y #delta y
 
-        distance = math.sqrt(dx**2+dy**2)#get distance
+        distance = math.sqrt(dx**2+dy**2) #get distance
 
         #check if distance!=0
         if distance == 0:
@@ -57,7 +78,7 @@ class Object_:
         
         force = G * self.mass * other.mass / distance**2 #force formula
         angle = math.atan2(dy, dx)
-        force_x = math.cos(angle) * force #get xForce
+        force_x = math.cos(angle) * force #get x force
         force_y = math.sin(angle) * force #get y force
 
         return force_x, force_y
@@ -70,22 +91,53 @@ class Object_:
         self.x += self.vx * TIMESTEP
         self.y += self.vy * TIMESTEP
 
-#create sun and earth
-sun = Object_(WIDTH / 2, HEIGHT / 2, 60, pg.Color('Yellow'), 1.989e30)
-earth = Object_(WIDTH / 2 + 128.781668823 * SCALE, HEIGHT / 2 + 82.781668823 * SCALE, 10, (0, 255, 0), 5.972e24)
+        if self.trail:
+            self.positions.append((self.x, self.y))
 
-#set y velocity for earth
-earth.vy = 29783
+sun = Object_(0, 0, 50, pg.Color('Yellow'), 1.989e30, name="Sun")
 
-objects = [sun, earth]
+planets = [
+    Object_(57.9e9, 0, 6, (169, 169, 169), 3.301e23, trail=True, name="Mercury"),   # Mercury
+    Object_(108.2e9, 0, 12, (218, 165, 32), 4.867e24, name="Venus"),  # Venus
+    Object_(149.6e9, 0, 20, (0, 255, 0), 5.972e24, trail=True, name="Earth"),    # Earth
+    Object_(227.9e9, 0, 10, (255, 0, 0), 0.642e24, name="Mars"),     # Mars
+    Object_(778.6e9, 0, 40, (255, 165, 0), 1898e24, name="Jupiter"),   # Jupiter
+    Object_(1433.5e9, 0, 34, (210, 180, 140), 568e24, name="Saturn"), # Saturn
+    Object_(2872.5e9, 0, 28, (0, 255, 255), 86.8e24, name="Uranus"),  # Uranus
+    Object_(4495.1e9, 0, 28, (0, 0, 255), 102e24, name="Neptune"),     # Neptune
+    Object_(5906.4e9, 0, 4, (128, 128, 128), 0.0146e24, name="Pluto") # Pluto
+]
+for planet in planets:
+    distance = planet.x
+    orbital_speed = (G * sun.mass / distance) ** 0.5
+    planet.vx = 0
+    planet.vy = orbital_speed
+
+objects = [sun] + planets
 
 run = True
 while run:
     for e in pg.event.get():
         if e.type == pg.QUIT:
             run = False
+        elif e.type == pg.MOUSEWHEEL:
+            mx, my = pg.mouse.get_pos()
+            world_before = screen_to_world(mx, my)
+
+            if e.y > 0:
+                SCALE /= 1.1
+            elif e.y < 0:
+                SCALE *= 1.1
+
+            world_after = screen_to_world(mx, my)
+            camera_x += world_before[0] - world_after[0]
+            camera_y += world_before[1] - world_after[1]
 
     sc.fill((255, 255, 255))
+
+    current_time_seconds += TIMESTEP
+    current_date = start_date + datetime.timedelta(seconds=current_time_seconds)
+
 
     for obj in objects:
         obj.fx = obj.fy = 0 #set to 0
@@ -93,13 +145,33 @@ while run:
         for other in objects:
             if obj != other:
                 fx, fy = obj.attraction(other) #updating attraction 
-#adding fx to object fx and ft to object fy
+#adding fx to object fx and fy to object fy
                 obj.fx += fx
                 obj.fy += fy
 
 # drawing and updating position 
     for obj in objects:
         obj.update_position()
-        obj.draw(sc)
+        obj.draw(sc, SCALE)
+
+    mouse_pos = pg.mouse.get_pos()
+    for obj in objects:
+        if is_mouse_over(obj, mouse_pos, SCALE):
+            name_surface = font.render(obj.name, True, (0,0,0))
+            sc.blit(name_surface, (mouse_pos[0]+10, mouse_pos[1]+10))
+
+    date_text = current_date.strftime("%d/%m/%Y %H:%M:%S")
+    date_surface = font.render(date_text, True, (0, 0, 0))
+    sc.blit(date_surface, (10, 10))
+    scale_text = f"1:{int(SCALE)}"
+    scale_surface = font.render(scale_text, True, (0, 0, 0))
+
+    text_width, text_height = scale_surface.get_size()
+    padding = 10
+    x = WIDTH - text_width - padding
+    y = HEIGHT - text_height - padding
+
+    sc.blit(scale_surface, (x, y))
+
 
     pg.display.flip()
